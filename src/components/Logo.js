d647d81f39fb55326c17802ff18df458a8175454
VictorDpnt <157797158+VictorDpnt@users.noreply.github.com>
2024-02-10 13:54:48 +0100

    Countries App REACT

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <div className="logo">
+        <img src="./international.png" alt="" />
+        <h3>Pays du monde</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem nam,
+        magni aliquam cupiditate neque temporibus sed amet impedit atque rem
+        itaque, accusamus quisquam quia at tempora architecto blanditiis sequi
+        id libero aut nemo inventore? Est facere tempora consequuntur debitis
+        cupiditate. Eos officiis sunt dignissimos hic quasi impedit. Harum
+        consequatur eos sit maiores, soluta cupiditate veritatis doloribus sunt
+        labore sint ut recusandae ullam. Eum pariatur rem, aut id tempore quasi
+        ad exercitationem, veniam iusto totam, voluptatum ducimus facilis eius.
+        Molestiae totam fugiat eligendi porro, vitae autem odio rem corporis
+        tenetur dignissimos eos ex impedit aut sed, rerum dolor, quam ad
+        voluptate!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+        quidem, aliquam ducimus impedit at debitis molestiae fuga temporibus
+        officia facere dolores corporis quasi reiciendis. Labore corporis
+        aperiam cum velit iure, maxime debitis, iste autem consequuntur beatae
+        vel unde eaque soluta quo! Deserunt suscipit, totam atque quisquam cum
+        maxime unde temporibus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
